08c8d77416637ed667ccafe99f4edaf41ddc14d2
Devin AF <devinayersfunata@gmail.com>
2025-09-28 12:47:58 +0700

    Main Activity

--- app/src/main/java/com/example/project_background_services/MainActivity.kt
+++ app/src/main/java/com/example/project_background_services/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.project_background_services
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +9,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val ACTION_SERVICE_STOPPED_JUMP = "nit2x.paba.backgroundservice.ACTION_SERVICE_STOPPED_JUMP"
+        const val REQUEST_CODE_PERMISSIONS = 101
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,4 +25,18 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+    private val permissionToRequest = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        arrayOf(
+            Manifest.permission.ACTIVITY_RECOGNITION,
+            Manifest.permission.POST_NOTIFICATIONS,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    } else {
+        arrayOf(
+            Manifest.permission.ACTIVITY_RECOGNITION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    }
+
+
 }
